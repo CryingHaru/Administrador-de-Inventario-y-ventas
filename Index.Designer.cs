138e7dfa1aa9dd3ec7de974f69b17b55965d4553
HaruCraft <luisaraniva2@gmail.com>
2024-09-06 21:49:12 -0600

    Agregar archivos de proyecto.

---
+++ Index.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Administrador_de_Inventario_y_ventas
+{
+    partial class Administador
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Administador
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Administador";
+            Text = "Administrador";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Index.cs
@@ -0,0 +1,10 @@
+namespace Administrador_de_Inventario_y_ventas
+{
+    public partial class Administador : Form
+    {
+        public Administador()
+        {
+            InitializeComponent();
+        }
+    }
+}
