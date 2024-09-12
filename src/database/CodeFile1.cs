4026bbb7ce441a7da5441e1d86ece4cb7b60cd9c
HaruCraft <luisaraniva2@gmail.com>
2024-09-11 18:35:48 -0600

    casi conexion

---
+++ src/database/CodeFile1.cs
@@ -0,0 +1,17 @@
+﻿//conector db
+using Microsoft.Data.SqlClient;
+namespace Administrador_de_Inventario_y_ventas { 
+//import sql server library
+    public class Conectordb
+    {
+        private string Server = "localhost";
+        private string Database = "ferreteriadb";
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection con = new SqlConnection("Server=" + Server + ";Database=" + Database + ";Trusted_Connection=True;");
+            
+        }
+        //metodo para abrir la conexion
+    }
+}
