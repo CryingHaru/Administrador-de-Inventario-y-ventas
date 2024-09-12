7348dc2ffad44d722a2063b5a3ed83ae16ee5ad2
HaruCraft <luisaraniva2@gmail.com>
2024-09-11 18:40:41 -0600

    Actualizar CodeFile1.cs

--- src/database/CodeFile1.cs
+++ src/database/CodeFile1.cs
@@ -1,17 +1,57 @@
-﻿//conector db
+//conector db
 using Microsoft.Data.SqlClient;
 namespace Administrador_de_Inventario_y_ventas { 
 //import sql server library
-    public class Conectordb
+    class Conectordb
     {
-        private string Server = "localhost";
-        private string Database = "ferreteriadb";
-
-        public SqlConnection Conectar()
+        protected string SQLcomando = "";
+        SqlConnection ConnectSQL = new SqlConnection();
+        SqlCommand Command = new SqlCommand();
+        SqlDataAdapter Adapter = new SqlDataAdapter();
+        private void Conectar()
         {
-            SqlConnection con = new SqlConnection("Server=" + Server + ";Database=" + Database + ";Trusted_Connection=True;");
-            
+            ConnectSQL = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=TiendaOnline;Integrated Security=True");
+            try
+            {
+                ConnectSQL.Open();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show("Ocurrio el siguiente error de SQL: " + error.Message);
+            }
         }
-        //metodo para abrir la conexion
+
+        protected DataTable Consultar()
+        {
+            DataTable Datos = new DataTable();
+            Conectar();
+            try
+            {
+                Adapter = new SqlDataAdapter(SQLcomando, ConnectSQL);
+                Adapter.Fill(Datos);
+                ConnectSQL.Close();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show("Ocurrio el siguiente error de SQL: " + error.Message);
+            }
+            return Datos;
+        }
+        protected bool Ejecutar()
+        {
+            Conectar();
+            try
+            {
+                Command = new SqlCommand(SQLcomando, ConnectSQL);
+                Command.ExecuteNonQuery();
+                ConnectSQL.Close();
+                return true;
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show("Ocurrio el siguiente error de SQL: " + error.Message);
+                return false;
+            }
+        }
     }
 }
