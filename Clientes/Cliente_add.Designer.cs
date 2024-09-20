28f3f2b81936704edfbb34164a6bb9844a3db330
Eduardo <Eduardo@EDUARDOFUENTES>
2024-09-19 19:33:10 -1200

    cliente_add

--- Clientes/Cliente_add.Designer.cs
+++ Clientes/Cliente_add.Designer.cs
@@ -33,14 +33,25 @@
             Cerrar = new AVI.RJControls.RJButton();
             label1 = new Label();
             Titulo = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
             SuspendLayout();
             // 
             // txtNombreCategoria
             // 
-            txtNombreCategoria.Location = new Point(273, 127);
+            txtNombreCategoria.Location = new Point(42, 156);
             txtNombreCategoria.Name = "txtNombreCategoria";
             txtNombreCategoria.Size = new Size(307, 27);
             txtNombreCategoria.TabIndex = 30;
+            txtNombreCategoria.TextChanged += txtNombreCategoria_TextChanged;
             // 
             // rjButton1
             // 
@@ -54,7 +65,7 @@
             rjButton1.FlatStyle = FlatStyle.Flat;
             rjButton1.ForeColor = Color.Black;
             rjButton1.ImageSize = new Size(20, 20);
-            rjButton1.Location = new Point(273, 229);
+            rjButton1.Location = new Point(42, 408);
             rjButton1.Margin = new Padding(3, 4, 3, 4);
             rjButton1.Name = "rjButton1";
             rjButton1.Size = new Size(114, 53);
@@ -75,7 +86,7 @@
             Cerrar.FlatStyle = FlatStyle.Flat;
             Cerrar.ForeColor = Color.Black;
             Cerrar.ImageSize = new Size(20, 20);
-            Cerrar.Location = new Point(466, 229);
+            Cerrar.Location = new Point(227, 408);
             Cerrar.Margin = new Padding(3, 4, 3, 4);
             Cerrar.Name = "Cerrar";
             Cerrar.Size = new Size(114, 53);
@@ -87,11 +98,12 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(273, 104);
+            label1.Location = new Point(42, 133);
             label1.Name = "label1";
-            label1.Size = new Size(112, 20);
+            label1.Size = new Size(69, 20);
             label1.TabIndex = 27;
-            label1.Text = "Nombre Marca:";
+            label1.Text = "Apellido:";
+            label1.Click += this.label1_Click;
             // 
             // Titulo
             // 
@@ -102,11 +114,104 @@
             Titulo.Size = new Size(0, 29);
             Titulo.TabIndex = 26;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(42, 90);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(307, 27);
+            textBox1.TabIndex = 31;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(42, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 20);
+            label2.TabIndex = 32;
+            label2.Text = "Nombre Cliente:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(42, 196);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 20);
+            label3.TabIndex = 33;
+            label3.Text = "Telelfono:";
+            label3.Click += label3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(42, 219);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(307, 27);
+            textBox2.TabIndex = 34;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(42, 279);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(307, 27);
+            textBox3.TabIndex = 35;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(42, 256);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 20);
+            label4.TabIndex = 36;
+            label4.Text = "Direccion:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(42, 316);
+            label5.Name = "label5";
+            label5.Size = new Size(37, 20);
+            label5.TabIndex = 37;
+            label5.Text = "DUI:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(42, 339);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(307, 27);
+            textBox4.TabIndex = 38;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(387, 238);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 20);
+            label6.TabIndex = 39;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(100, 30);
+            label7.Name = "label7";
+            label7.Size = new Size(178, 29);
+            label7.TabIndex = 40;
+            label7.Text = "Nuevo Cliente";
+            // 
             // Cliente_add
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(890, 496);
+            ClientSize = new Size(373, 496);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
             Controls.Add(txtNombreCategoria);
             Controls.Add(rjButton1);
             Controls.Add(Cerrar);
@@ -114,6 +219,7 @@
             Controls.Add(Titulo);
             Name = "Cliente_add";
             Text = "Cliente_add";
+            Load += this.Cliente_add_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -125,5 +231,15 @@
         private AVI.RJControls.RJButton Cerrar;
         private Label label1;
         private Label Titulo;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label6;
+        private Label label7;
     }
 }
--- Clientes/Cliente_add.cs
+++ Clientes/Cliente_add.cs
@@ -16,5 +16,15 @@ namespace Administrador_de_Inventario_y_ventas.Clientes
         {
             InitializeComponent();
         }
+
+        private void txtNombreCategoria_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
